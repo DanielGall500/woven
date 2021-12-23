e86eee3eab2acc57ee227dd9b0c3a9cea6a5ff7d
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-12-23 20:13:44 +0000

    inheritance updates

--- src/model/nmt_model.py
+++ src/model/nmt_model.py
@@ -8,15 +8,27 @@ used and deployed in python.
 """
 from transformers import MarianMTModel, MarianTokenizer
 
-src_text = [">>deu<< Happy Christmas!"]
+class NMTModel:
+	def __init__(self, source, target):
+		model_name = "Helsinki-NLP/opus-mt-{}-{}".format(source, target)
 
-model_name = 'Helsinki-NLP/opus-mt-en-gem'
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-print(tokenizer.supported_language_codes)
+		#The tokenizer converts the text to a more NN-suitable
+		#format, as it cannot take raw text.
+		#Uses sentencepiece library
+		self.tokenizer = MarianTokenizer.from_pretrained(model_name)
 
-model = MarianMTModel.from_pretrained(model_name)
-translated = model.generate(**tokenizer(src_text, return_tensors="pt", padding=True))
-decoded = [tokenizer.decode(t) for t in translated]
-print(decoded)
+		#Load the model
+		self.model = MarianMTModel.from_pretrained(model_name)
+
+	def translate(self, src_text):
+		#Encode: convert to format suitable for an NN
+		encode_src = self.tokenizer(src_text, return_tensors="pt", padding=True)
+
+		#Feed forward through NMT model
+		translation = self.model.generate(**encode_src)
+
+		#Decode: convert to regular string
+		decode_target = [self.tokenizer.decode(t) for t in translation]
+		return decode_target
 
 
