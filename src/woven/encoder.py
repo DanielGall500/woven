2f45d1dafdc85115d95b881c7abb07eff17bf317
unknown <daniel.gallagher1@ucdconnect.ie>
2022-02-24 13:07:49 +0000

    variation

--- src/woven/encoder.py
+++ src/woven/encoder.py
@@ -1,5 +1,5 @@
 import shap
-from encodable import WOVEncodable
+from woven.encodable import WOVEncodable
 import numpy as np
 
 """
@@ -10,17 +10,23 @@ the resulting Shapley values in a WOVEncodable object.
 """
 
 class WOVEncoder:
-    explainer_file_path = "explainer/partition_explainer.pickle"
-
+    explainer_file_path = "woven/explainer/partition_explainer.pickle"
+    encodable = None
+    
     def __init__(self):
         #Load the Pre-trained SHAP Partition Explainer
         with open(self.explainer_file_path, 'rb+') as exp_file:
             self.explainer = shap.Explainer.load(exp_file)
 
 
-    def encode(self, E: str):
+    def encode(self, E: str, \
+            min_confidence_threshold=20, \
+            multi_confidence_threshold=10, \
+            merge_tokens=True):
         if self._is_valid_input(E):
-            shap_exp = self.explainer(E)
+            shap_exp = self.explainer([E])
+        else:
+            raise Exception("Invalid Input")
 
         #Save relevant information to encode word order
         input_tokenised = shap_exp.feature_names[0]
@@ -30,12 +36,24 @@ class WOVEncoder:
         shap_values = np.squeeze(shap_values)
         
         #Store relevant information in encodable object
-        encodable = WOVEncodable(t_inp=input_tokenised,
+        self.encodable = WOVEncodable(org_input=E,
+                t_inp=input_tokenised,
                 t_out=output_tokenised,
                 shap_values=shap_values)
+        
+        self.encoding = self.encodable.get_encoding(\
+                l=min_confidence_threshold,\
+                theta=multi_confidence_threshold,\
+                merged=merge_tokens)
+        
+        return self.encodable
 
-        return encodable
+    def get_variation(self):
+        if self.encodable is None:
+            return None
 
+        enc = self.encoding
+        return enc
 
     def _is_valid_input(self, E):
         return True
