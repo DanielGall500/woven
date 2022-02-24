5d8c4f4ec1f0fc07d7ae5421a6ebb390249ac1d0
unknown <daniel.gallagher1@ucdconnect.ie>
2022-02-24 13:07:29 +0000

    basic testing for encoder

--- src/tests/test_encoder.py
+++ src/tests/test_encoder.py
@@ -2,7 +2,7 @@ import unittest
 from woven.encoder import WOVEncoder
 
 class TestWOVEncoder(unittest.TestCase):
-    example_str = "I hire him on Monday"
+    example_str = "I ate it yesterday"
 
     def test_variation(self):
         encoder = WOVEncoder()
