f91dda0ea78558428bfecd8e89792aeb87532038
unknown <daniel.gallagher1@ucdconnect.ie>
2022-03-11 15:18:51 +0100

    get/post requests handled

--- src/app/webapp.py
+++ src/app/webapp.py
@@ -1,10 +1,14 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("index.html", gender="female", name="Daniel")
-
+    if request.method == "GET":
+        return render_template("index.html", translation="None") 
+    else:
+        print(request.form.keys())
+        text_input = request.form["inputText"]
+        return render_template("index.html", translation=text_input)
 if __name__ == "__main__":
     app.run(debug=True)
