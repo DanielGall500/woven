d72c26419dd50e817c74831cadafa988ad30560d
unknown <daniel.gallagher1@ucdconnect.ie>
2022-03-14 18:30:21 +0100

    vis page added

--- src/run.py
+++ src/run.py
@@ -19,17 +19,16 @@ def home():
         out_text = " ".join([x for x in encodable.t_out])
         itokens = encodable.merged_inp
         otokens = encodable.t_out
-        print("Encoding:")
-        print("Input: ", itokens)
-        print("Output: ", otokens)
-        print(encoding)
-        print("----")
-        grph = create_graph(itokens,otokens,encoding)
-        grph.show('nx.html')
+        grph = create_graph(itokens,otokens,icolours,ocolours,encoding)
+        grph.write_html('templates/vis.html')
         return render_template("index.html", translation=out_text, itokens=itokens, \
                 otokens=otokens, icolours=icolours,\
                 ocolours=ocolours, num_input_tokens=range(len(itokens)),\
                 num_output_tokens=range(len(otokens)))
+
+@app.route("/vis", methods=["POST", "GET"])
+def vis():
+    return render_template("vis.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
