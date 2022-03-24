e8d0a79baf68818184a648541dab0c905d559600
unknown <daniel.gallagher1@ucdconnect.ie>
2022-03-24 19:28:42 +0000

    minor changes

--- src/run.py
+++ src/run.py
@@ -2,9 +2,36 @@ from flask import Flask, redirect, url_for, render_template, request
 from woven.encoder import WOVEncoder
 from connections import make_colour_connections, create_graph
 from pyvis.network import Network
+import numpy as np
 
 app = Flask(__name__)
 encoder = WOVEncoder()
+
+def remove_punc(itokens, otokens, encoding):
+    test_itokens = np.array(itokens)
+    test_encoding = np.array(encoding)
+    itokens_punc = np.where(test_itokens == '.')
+    for indx in itokens_punc[0]:
+        test_itokens = np.delete(test_itokens,indx)
+        test_encoding = np.delete(test_encoding,indx,0)
+
+    itokens_punc = np.where(test_itokens == ',')
+    for indx in itokens_punc[0]:
+        test_itokens = np.delete(test_itokens,indx)
+        test_encoding = np.delete(test_encoding,indx,0)
+
+    test_otokens = np.array(otokens)
+    otokens_punc = np.where(test_otokens=='.')
+    for indx in otokens_punc[0]:
+        test_otokens = np.delete(test_otokens,indx)
+        test_encoding = np.delete(test_encoding,indx,1)
+
+    otokens_punc = np.where(test_otokens==',')
+    for indx in otokens_punc[0]:
+        test_otokens = np.delete(test_otokens,indx)
+        test_encoding = np.delete(test_encoding,indx,1)
+
+    return test_itokens.tolist(), test_otokens.tolist(), test_encoding
 
 def create_html_table_format(itokens,otokens,encoding):
     table = []
@@ -32,10 +59,12 @@ def home():
         encoding = encoder.encoding
 
         #Main parameters for HTML
-        icolours, ocolours = make_colour_connections(encoding)
-        out_text = " ".join([x for x in encodable.t_out])
         itokens = encodable.merged_inp
         otokens = encodable.t_out
+        itokens, otokens, encoding = remove_punc(itokens, otokens, encoding)
+        icolours, ocolours = make_colour_connections(encoding)
+        out_text = " ".join([x for x in encodable.t_out])
+
         table_encoding = create_html_table_format(itokens,otokens,encoding)
 
         #Create the vis
